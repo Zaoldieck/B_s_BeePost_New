183c1c2738a2844adbbe37e8e50009a266a4201d
Bpetiii <87673385+Bpetiii@users.noreply.github.com>
2025-01-11 11:15:30 +0100

    Initial commit

---
+++ src/main/java/biraw/online/b_s_BeePost/B_s_BeePost.java
@@ -0,0 +1,17 @@
+package biraw.online.b_s_BeePost;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class B_s_BeePost extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
